faecc4d29208bb1715d0afb536b72ddd31cd1696
makoto hata <makotohata0429@gmail.com>
2024-07-12 18:15:57 +0900

    service container

---
+++ app/app/Http/Controllers/LifeCycleTestController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LifeCycleTestController extends Controller
+{
+    public function showServiceContainerTest() {
+        app()->bind('lifeCycleTest', function(){
+            return 'ライフサイクルのテスト';
+        });
+
+        $test = app()->make('lifeCycleTest');
+
+        // サービスコンテナなしのパターン
+        // インスタンス化したMessageクラスをSampleクラスに渡す必要がある。（MessageとSampleは依存関係にある）
+        // $message = new Message();
+        // $sample = new Sample($message);
+        // $sample->run();
+
+        // サービスコンテナありのパターン
+        // インスタンス化せずに、依存関係も解決した状態で呼び出せる
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
+
+        dd($test, app());
+    }
+}
+
+class Sample
+{
+    public $message;
+    public function __construct(Message $message){
+        $this->message = $message;
+    }
+    public function run(){
+        $this->message->send();
+    }
+}
+
+class Message
+{
+    public function send(){
+        echo('メッセージ表示');
+    }
+}
