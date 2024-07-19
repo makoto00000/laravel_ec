5e76ea79e5ddb9ada98ada482cfce5455a5414e4
makoto hata <makotohata0429@gmail.com>
2024-07-19 20:48:35 +0900

    Owner Store

--- app/app/Http/Controllers/Admin/OwnersController.php
+++ app/app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Models\Owner; // Eloquent
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB; // QueryBuilder
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use App\Models\Admin;
 
 class OwnersController extends Controller
 {
@@ -59,7 +62,22 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message', 'オーナーを新規登録しました。')
+        ;
     }
 
     /**
