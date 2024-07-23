58f9a6628cb923803f49be5389b7c1859917a6c6
makoto hata <makotohata0429@gmail.com>
2024-07-23 19:17:28 +0900

    pagination

--- app/database/seeders/OwnerSeeder.php
+++ app/database/seeders/OwnerSeeder.php
@@ -34,6 +34,24 @@ class OwnerSeeder extends Seeder
                 'password' => Hash::make('password123'),
                 'created_at' => '2024/07/16 11:11:11'
             ],
+            [
+                'name' => 'test-owner4',
+                'email' => 'test-owner4@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2024/07/16 11:11:11'
+            ],
+            [
+                'name' => 'test-owner5',
+                'email' => 'test-owner5@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2024/07/16 11:11:11'
+            ],
+            [
+                'name' => 'test-owner6',
+                'email' => 'test-owner6@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2024/07/16 11:11:11'
+            ],
         ]);
     }
 }
