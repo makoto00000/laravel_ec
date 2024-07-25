35c29728c87677b0636b5dc006bcfb362b45c243
makoto hata <makotohata0429@gmail.com>
2024-07-25 18:52:28 +0900

    middleware, abort(404)

---
+++ app/app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+
+class ShopController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        // ログインしているオーナー以外のshop情報を表示させない
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('shop');
+            if(!is_null($id)){
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerId;
+                $ownerId = Auth::id();
+                if($shopId !== $ownerId){
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+
+    public function index() {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', $ownerId)->get();
+        return view('owner.shops.index', compact('shops'));
+    }
+    public function edit(Request $request, $id) {
+        dd(Shop::findOrFail($id));
+    }
+    public function update(Request $request, $id) {
+
+    }
+}
