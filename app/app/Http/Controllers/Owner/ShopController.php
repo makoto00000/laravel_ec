00aa4c998ba496863d35480e0fd702e9fb0aebe6
makoto hata <makotohata0429@gmail.com>
2024-07-27 21:42:49 +0900

    upload image without resize

--- app/app/Http/Controllers/Owner/ShopController.php
+++ app/app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -34,9 +35,14 @@ class ShopController extends Controller
         return view('owner.shops.index', compact('shops'));
     }
     public function edit(Request $request, $id) {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        return view('owner.shops.edit', compact('shop'));
     }
     public function update(Request $request, $id) {
-
+        $imageFile = $request->image;
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            Storage::putFile('public/shops', $imageFile);
+        }
+        return redirect()->route('owner.shops.index');
     }
 }
